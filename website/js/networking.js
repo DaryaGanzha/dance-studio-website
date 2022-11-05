639087f28c647890a4016e2bc86fc9fc20e5fff4
DaryaGanzha <ganzadara@gmail.com>
2022-11-05 17:24:29 +0300

    feat: add preloader feat: add condition for request feat: add error message

--- website/js/networking.js
+++ website/js/networking.js
@@ -1,89 +1,15 @@
-// $(document).ready(function() {
-// //Preloader
-//     let preloaderFadeOutTime = 500;
-//     function hidePreloader() {
-//         let preloader = $('.spinner-wrapper');
-//         preloader.fadeOut(preloaderFadeOutTime);
-// }
-//     hidePreloader();
-// });
-//
-// function createNode(element) {
-//     return document.createElement(element);
-// }
-//
-// function append(parent, el) {
-//     return parent.appendChild(el);
-// }
-//
-// const ul = document.getElementById('reviews');
-// const url = "https://jsonplaceholder.typicode.com/posts/1";
-// const url = "https://randomuser.me/api/?results=10";
-//
-// fetch(url)
-//  .then((resp) => resp.json())
-//  .then(function(data) {
-//      let reviews = data.result;
-//      // console.log(reviews)
-//      return reviews.map(function(review) {
-//          // let li = createNode('li');
-//          // let p = createNode('p');
-//          //
-//          // append(li, p);
-//          // append(ul, li);
-//          let li = createNode('li');
-//          let img = createNode('img');
-//          let span = createNode('span');
-//          img.src = review.pictureInPictureElement.media;
-//          span.innerHTML = `${review.name.firstChild} ${review.name.lastChild}`;
-//          append(li, img);
-//          append(li, span);
-//          append(ul, li);
-//      })
-//  })
-//  .catch(function (error) {
-//      console.log(error);
-//  });
-
-// function sendRequest(method, url) {
-//     return fetch(url)
-//         .then(resp => resp.json())
-// }
-//
-// sendRequest('GET', url)
-//  .then(function(data) {
-//      console.log(data);
-//      obj = JSON.parse(data);
-//      console.log(obj.userID);
-//      let reviews = data.result;
-//      //console.log(reviews);
-//      return reviews.map(function(review) {
-//          let li = createNode('li');
-//          let p = createNode('p');
-//          obj = JSON.parse(data);
-//          p.innerHTML = obj
-//          append(li, p);
-//          append(ul, li);
-//      })
-//  })
-//  .catch(error => console.log(error))
-
-const url = 'https://jsonplaceholder.typicode.com/posts?_limit=10'
+const url = 'https://jsonplaceholder.typicode.com/comments?_limit=10'
 const postsList = document.querySelector('.posts__list');
 
 const state = {
     posts: [],
 }
 
-const createPost = (post, index) => `
+const createPost = (post) => `
    <div class="post">
       <div class="post__wrapper">
-         <h1 class="wrapper__title">${post.title}</h1>
+         <h1 class="wrapper__title">${post.name}</h1>
          <div class="wrapper__body">${post.body}</div>
-      </div>
-      <div class="post__buttons">
-         <button class="buttons__edit" onclick="editPost(${index})">Edit</button>
-         <button class="buttons__delete" onclick="deletePost(${index})">Delete</button>
       </div>
    </div>
 `
@@ -91,7 +17,7 @@ const createPost = (post, index) => `
 const fillPostsList = (posts) => {
     postsList.innerHTML = "";
     if (posts.length) {
-        posts.forEach((post, index) => postsList.innerHTML += createPost(post, index));
+        posts.forEach((post) => postsList.innerHTML += createPost(post));
     }
 }
 
@@ -100,12 +26,37 @@ window.addEventListener("load", async () => {
     fillPostsList(state.posts);
 })
 
+function getRandomNum() {
+    let num = Math.floor(Math.random() * 3) + 3;
+    console.log(num);
+    return num;
+}
+
+function showPreloader() {
+    document.body.classList.add('.preloader');
+}
+
 function getPostRequest() {
+    showPreloader();
     return fetch(url, {
         headers: {
-            "Content-type" : "application/json; charset=UTF-8"
+            "Content-type": "application/json; charset=UTF-8"
         }
     })
+        .then((res) => {
+            if (res.status < 100 * getRandomNum()) {
+                return res;
+            }
+            throw new Error(res.statusText);
+        })
         .then((res) => res.json())
-        .then((posts) => state.posts = state.posts.concat(posts))
+        .then((posts) =>  {
+            state.posts = state.posts.concat(posts);
+            document.querySelector(".preloader").style.display = "none";
+        })
+        .catch(function (error) {
+            console.log(error);
+            let err = document.getElementById('#error');
+            err.innerHTML = "Что-то пошло не так :(";
+        })
 }
